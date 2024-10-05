35a831a6c3c07cae621e8fb66f373cf8bd790362
LifeDanik(Danylo Pavlov) <129084010+FireLifeDanik28@users.noreply.github.com>
2024-10-05 21:04:05 +0200

    Update Form1.cs

--- Pogoda/Form1.cs
+++ Pogoda/Form1.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Text.Json;
+using System.Windows.Forms;
 
 namespace Pogoda
 {
@@ -8,6 +12,7 @@ namespace Pogoda
         {
             InitializeComponent();
         }
+
         public class Current
         {
             public string time { get; set; }
@@ -38,16 +43,131 @@ namespace Pogoda
             public CurrentUnits current_units { get; set; }
             public Current current { get; set; }
         }
-        private void button1_Click(object sender, EventArgs e)
+        //refresh
+        private async void button1_Click(object sender, EventArgs e)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://api.open-meteo.com/v1/forecast?latitude=54.36&longitude=18.64&current=temperature_2m,relative_humidity_2m,weather_code");
-            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
-            string result = response.Content.ReadAsStringAsync().Result;
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.open-meteo.com/v1/forecast");
+                string url = "?" +
+                             "latitude=54.36&" +
+                             "longitude=18.64&" +
+                             "current=temperature_2m,relative_humidity_2m,weather_code";
+                HttpResponseMessage response = await client.GetAsync(url);
+                string result = await response.Content.ReadAsStringAsync();
+                Root? weatherForecast = JsonSerializer.Deserialize<Root>(result);
 
-            Current? weatherForecast = JsonSerializer.Deserialize<Current>(result);
+                if (weatherForecast?.current != null)
+                {
+                    string[] Separate = weatherForecast.current.time.Split('T');
+                    string apiTime = Separate[1];
+                    string summary;
+                    switch (weatherForecast.current.weather_code)
+                    {
+                        case 0:
+                            summary = "Clear Sky";
+                            break;
+                        case 1:
+                            summary = "Mainly clear";
+                            break;
+                        case 2:
+                            summary = "Partly cloudy";
+                            break;
+                        case 3:
+                            summary = "Overcast";
+                            break;
+                        case 45:
+                            summary = "Fog";
+                            break;
+                        case 48:
+                            summary = "Depositing rime fog";
+                            break;
+                        case 51:
+                            summary = "Light drizzle";
+                            break;
+                        case 53:
+                            summary = "Moderate drizzle";
+                            break;
+                        case 55:
+                            summary = "Dense drizzle";
+                            break;
+                        case 56:
+                            summary = "Light freezing drizzle";
+                            break;
+                        case 57:
+                            summary = "Heavy freezing drizzle";
+                            break;
+                        case 61:
+                            summary = "Slight rain";
+                            break;
+                        case 63:
+                            summary = "Moderate rain";
+                            break;
+                        case 65:
+                            summary = "Heavy rain";
+                            break;
+                        case 66:
+                            summary = "Light freezing rain";
+                            break;
+                        case 67:
+                            summary = "Heavy freezing rain";
+                            break;
+                        case 71:
+                            summary = "Slight snowfall";
+                            break;
+                        case 73:
+                            summary = "Moderate snowfall";
+                            break;
+                        case 75:
+                            summary = "Heavy snowfall";
+                            break;
+                        case 77:
+                            summary = "Snow grains";
+                            break;
+                        case 80:
+                            summary = "Slight rain shower";
+                            break;
+                        case 81:
+                            summary = "Moderate rain shower";
+                            break;
+                        case 82:
+                            summary = "Violent rain shower";
+                            break;
+                        case 85:
+                            summary = "Slight snow shower";
+                            break;
+                        case 86:
+                            summary = "Heavy snow shower";
+                            break;
+                        case 95:
+                            summary = "Thunderstorm";
+                            break;
+                        case 96: case 99:
+                            summary = "Thunderstorm with slight and heavy hail";
+                            break;
+                        default:
+                            summary = "End of the World";
+                            break;
+                    }
+                    label1.Text = $"Temperature: {weatherForecast.current.temperature_2m}ï¿½C";
+                    label2.Text = $"Humidity: {weatherForecast.current.relative_humidity_2m}%";
+                    label3.Text = $"Summary: {summary}";
+                    label4.Text = $"Last api refresh: {apiTime}";
 
-            richTextBox1.Text = $"Temperature: {weatherForecast?.temperature_2m}°C, Humidity: {weatherForecast?.relative_humidity_2m}%, Weather Code: {weatherForecast?.weather_code}";
+                }
+                else
+                {
+                    label1.Text = "Error 404";
+                    label2.Text = "Error 404";
+                    label3.Text = "Error 404";
+                    label4.Text = "Error 404";
+                }
+            }
+        }
+        //15 minutowy timer
+        private void refreshTimer_Tick(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
         }
     }
 }
